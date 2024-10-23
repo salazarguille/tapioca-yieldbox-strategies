36106a868f20884f19b2f69af1fadcb1259dbf32
Guillermo Salazar <guillesalazar@gmail.com>
2024-10-22 18:11:07 -0600

    feat: add tests (fuzz).

---
+++ test/fuzz/yvToken/yvTokenStrategyConstructor.t.sol
@@ -0,0 +1,215 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.22;
+
+import {Test} from "forge-std/Test.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {yvTokenStrategy} from "../../../contracts/yvToken/yvTokenStrategy.sol";
+import {Errors} from "../../../contracts/utils/Errors.sol";
+import {MockDecimalsERC20} from "../../../contracts/mocks/MockDecimalsERC20.sol";
+import {MockERC4626} from "../../../contracts/mocks/MockERC4626.sol";
+import {IVault} from "../../../contracts/interfaces/yearnv3/IVault.sol";
+
+contract yvTokenStrategyConstructorTest is Test {
+    IERC20 public token;
+    IVault public yvToken;
+
+    function setUp() external {
+        token = IERC20(address(new MockDecimalsERC20("MockToken", "MT", 18)));
+        yvToken = IVault(address(new MockERC4626(token)));
+    }
+
+    function test_RevertWhen_The_yieldBoxIsEmpty(
+        address _cluster,
+        address _token,
+        address _yvToken,
+        address _owner,
+        uint256 _depositThreshold
+    ) external {
+        vm.assume(_cluster != address(0));
+        vm.assume(_token != address(0));
+        vm.assume(_yvToken != address(0));
+        vm.assume(_owner != address(0));
+        vm.assume(_depositThreshold > 0);
+        address yieldBox = address(0);
+
+        vm.expectRevert(abi.encodeWithSelector(Errors.AddressNotValid.selector, yieldBox));
+        new yvTokenStrategy(yieldBox, _cluster, _token, _yvToken, _owner, _depositThreshold);
+    }
+
+    function test_RevertWhen_The_clusterIsEmpty(
+        address _yieldBox,
+        address _token,
+        address _yvToken,
+        address _owner,
+        uint256 _depositThreshold
+    ) external {
+        vm.assume(_yieldBox != address(0));
+        vm.assume(_token != address(0));
+        vm.assume(_yvToken != address(0));
+        vm.assume(_owner != address(0));
+        vm.assume(_depositThreshold > 0);
+        address cluster = address(0);
+
+        vm.expectRevert(abi.encodeWithSelector(Errors.AddressNotValid.selector, cluster));
+        new yvTokenStrategy(_yieldBox, cluster, _token, _yvToken, _owner, _depositThreshold);
+    }
+
+    function test_RevertWhen_The_yvTokenIsEmpty(
+        address _yieldBox,
+        address _cluster,
+        address _token,
+        address _owner,
+        uint256 _depositThreshold
+    )
+        external
+    {
+        vm.assume(_yieldBox != address(0));
+        vm.assume(_cluster != address(0));
+        vm.assume(_token != address(0));
+        vm.assume(_owner != address(0));
+        vm.assume(_depositThreshold > 0);
+        address _yvToken = address(0);
+
+        vm.expectRevert(abi.encodeWithSelector(Errors.TokenNotValid.selector, _yvToken));
+        new yvTokenStrategy(_yieldBox, _cluster, _token, _yvToken, _owner, _depositThreshold);
+    }
+
+    function test_RevertWhen_The_ownerIsEmpty(
+        address _yieldBox,
+        address _cluster,
+        address _token,
+        address _yvToken,
+        uint256 _depositThreshold
+    ) external {
+        vm.assume(_yieldBox != address(0));
+        vm.assume(_cluster != address(0));
+        vm.assume(_token != address(0));
+        vm.assume(_yvToken != address(0));
+        vm.assume(_depositThreshold > 0);
+        address owner = address(0);
+
+        vm.expectRevert(abi.encodeWithSelector(Errors.AddressNotValid.selector, owner));
+        new yvTokenStrategy(_yieldBox, _cluster, _token, _yvToken, owner, _depositThreshold);
+    }
+
+    function test_RevertWhen_TheDepositThresholdIsZero(address _yieldBox, address _cluster, address _owner) external {
+        vm.assume(_yieldBox != address(0));
+        vm.assume(_cluster != address(0));
+        vm.assume(_owner != address(0));
+        uint256 _depositThreshold = 0;
+
+        vm.expectRevert(abi.encodeWithSelector(Errors.AmountNotValid.selector, _depositThreshold));
+        new yvTokenStrategy(_yieldBox, _cluster, address(token), address(yvToken), _owner, _depositThreshold);
+    }
+
+    modifier givenTokenIsNotEmpty() {
+        _;
+    }
+
+    function test_RevertWhen_TheTokenDecimalsIsNotEqualTo18(
+        address _yieldBox,
+        address _cluster,
+        address _owner,
+        uint256 _depositThreshold,
+        uint8 _decimals
+    ) external givenTokenIsNotEmpty {
+        vm.assume(_decimals > 0);
+        vm.assume(_decimals != 18);
+        vm.assume(_yieldBox != address(0));
+        vm.assume(_cluster != address(0));
+        vm.assume(_owner != address(0));
+        vm.assume(_depositThreshold > 0);
+
+        address _token = address(new MockDecimalsERC20("MockToken", "MT", _decimals));
+        address _yvToken = address(new MockERC4626(IERC20(_token)));
+
+        vm.expectRevert(abi.encodeWithSelector(Errors.ValueNotValid.selector, _decimals));
+        new yvTokenStrategy(_yieldBox, _cluster, _token, _yvToken, _owner, _depositThreshold);
+    }
+
+    modifier given_yvTokensAssetIsNotEmpty() {
+        _;
+    }
+
+    function test_RevertWhen__tokenIsEmpty(
+        address _yieldBox,
+        address _cluster,
+        address _owner,
+        uint256 _depositThreshold
+    ) external given_yvTokensAssetIsNotEmpty {
+        vm.assume(_yieldBox != address(0));
+        vm.assume(_cluster != address(0));
+        vm.assume(_owner != address(0));
+        vm.assume(_depositThreshold > 0);
+        address _token = address(0);
+
+        vm.expectRevert(abi.encodeWithSelector(Errors.TokenNotValid.selector, _token));
+        new yvTokenStrategy(
+            _yieldBox,
+            _cluster,
+            _token,
+            address(yvToken),
+            _owner,
+            _depositThreshold
+        );
+    }
+
+    modifier when_tokenIsNotEmpty() {
+        _;
+    }
+
+    function test_Given_tokenIsEqualTo_yvTokensAsset(
+        address _yieldBox,
+        address _cluster,
+        address _owner,
+        uint256 _depositThreshold
+    ) external given_yvTokensAssetIsNotEmpty when_tokenIsNotEmpty {
+        vm.assume(_yieldBox != address(0));
+        vm.assume(_cluster != address(0));
+        vm.assume(_owner != address(0));
+        vm.assume(_depositThreshold > 0);
+
+        vm.startPrank(_owner);
+        yvTokenStrategy strategy = new yvTokenStrategy(
+            _yieldBox,
+            _cluster,
+            address(token),
+            address(yvToken),
+            _owner,
+            _depositThreshold
+        );
+        vm.stopPrank();
+
+        assertEq(address(strategy.yieldBox()), _yieldBox, "Yield box is not valid");
+        assertEq(address(strategy.asset()), address(token), "Token is not valid");
+        assertEq(address(strategy.yvToken()), address(yvToken), "yvToken is not valid");
+        assertEq(strategy.name(), "tyvDai", "Name is not valid");
+        assertEq(strategy.description(), "yvDai strategy for dai assets", "Description is not valid");
+        assertEq(strategy.depositThreshold(), _depositThreshold, "Deposit threshold is not valid");
+    }
+
+    function test_RevertGiven__tokenIsNotEqualTo_yvTokensAsset(
+        address _yieldBox,
+        address _cluster,
+        address _token,
+        address _owner,
+        uint256 _depositThreshold
+    ) external given_yvTokensAssetIsNotEmpty when_tokenIsNotEmpty {
+        vm.assume(_yieldBox != address(0));
+        vm.assume(_cluster != address(0));
+        vm.assume(_token != address(0));
+        vm.assume(_token != address(token));
+        vm.assume(_owner != address(0));
+        vm.assume(_depositThreshold > 0);
+
+        vm.expectRevert(abi.encodeWithSelector(Errors.TokensNotMatch.selector, address(token), _token));
+        new yvTokenStrategy(
+            _yieldBox,
+            _cluster,
+            _token,
+            address(yvToken),
+            _owner,
+            _depositThreshold
+        );
+    }
+}
---
+++ test/fuzz/yvToken/yvTokenStrategyRescueEth.t.sol
@@ -0,0 +1,148 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.22;
+
+import {Test} from "forge-std/Test.sol";
+import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+import {IWrappedNative} from "yieldbox/interfaces/IWrappedNative.sol";
+import {IStrategy} from "yieldbox/interfaces/IStrategy.sol";
+import {Pearlmit} from "tap-utils/pearlmit/Pearlmit.sol";
+import {YieldBoxURIBuilder} from "yieldbox/YieldBoxURIBuilder.sol";
+import {yvTokenStrategy} from "../../../contracts/yvToken/yvTokenStrategy.sol";
+import {YieldBox} from "yieldbox/YieldBox.sol";
+import {Errors} from "../../../contracts/utils/Errors.sol";
+import {MockERC20} from "../../../contracts/mocks/MockERC20.sol";
+import {MockERC4626} from "../../../contracts/mocks/MockERC4626.sol";
+import {EthRescuable} from "../../../contracts/utils/EthRescuable.sol";
+import {NoTransferEthContract} from "../utils/NoTransferEthContract.sol";
+import {TransferEthContract} from "../utils/TransferEthContract.sol";
+
+contract yvTokenStrategyRescueEthTest is Test {
+    using Address for address;
+    yvTokenStrategy public strategy;
+    address public owner;
+
+    function setUp() external {
+        owner = address(0x1234567890);
+        uint256 depositThreshold = 10000e18;
+        YieldBox yieldBox = new YieldBox(
+            IWrappedNative(address(0x999999999999)),
+            YieldBoxURIBuilder(address(0x888888888888)),
+            Pearlmit(address(0x777777777777)),
+            address(this)
+        );
+        IERC20 token = IERC20(address(new MockERC20("MockToken", "MT")));
+        IERC4626 yvToken = new MockERC4626(token);
+        vm.startPrank(owner);
+        strategy = new yvTokenStrategy(
+            address(yieldBox),
+            address(0x666666666666),
+            address(token),
+            address(yvToken),
+            owner,
+            depositThreshold
+        );
+        vm.stopPrank();
+    }
+
+    function test_RevertWhen_TransactionSenderIsNotTheOwner(
+        address _sender,
+        address _to,
+        uint256 _amount
+    ) external {
+        vm.assume(_sender != owner);
+        vm.assume(_sender != address(0));
+        vm.assume(_amount > 0);
+        vm.assume(_to != address(0));
+
+        vm.startPrank(_sender);
+        vm.expectRevert("Ownable: caller is not the owner");
+        strategy.rescueEth(_amount, _to);
+        vm.stopPrank();
+    }
+
+    modifier whenTransactionSenderIsTheOwner() {
+        _;
+    }
+
+    function test_GivenTheAmountIsGreaterThanZero(
+        address _to,
+        uint256 _amount
+    ) external whenTransactionSenderIsTheOwner {
+        vm.assume(_amount > 0);
+        vm.assume(_to != address(0));
+        vm.assume(!_to.isContract());
+        uint256 toBalanceBefore = _to.balance;
+
+        vm.deal(address(strategy), _amount);
+
+        vm.startPrank(strategy.owner());
+        vm.expectEmit(true, true, false, true, address(strategy));
+        emit EthRescuable.EthRescued(_to, _amount);
+        strategy.rescueEth(_amount, _to);
+        vm.stopPrank();
+
+        uint256 toBalanceAfter = _to.balance;
+        assertEq(toBalanceAfter, toBalanceBefore + _amount, "Invalid balance");
+    }
+
+    function test_RevertGiven_TheAmountIsZero(
+        address _to
+    ) external whenTransactionSenderIsTheOwner {
+        vm.assume(_to != address(0));
+        uint256 _amount = 0;
+
+        vm.startPrank(strategy.owner());
+        vm.expectRevert(Errors.EmptyAmount.selector);
+        strategy.rescueEth(_amount, _to);
+        vm.stopPrank();
+    }
+
+    modifier whenToAddressIsAContract() {
+        _;
+    }
+
+    modifier givenTheTransactionSenderIsTheOwner() {
+        _;
+    }
+
+    function test_RevertWhen_TheCallToTheContractFails(
+        uint256 _amount
+    ) external whenToAddressIsAContract givenTheTransactionSenderIsTheOwner {
+        vm.assume(_amount > 0);
+        address _to = address(new NoTransferEthContract());
+
+        vm.startPrank(strategy.owner());
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Errors.TransferFailed.selector,
+                strategy.ETH_TOKEN_ADDRESS(),
+                _to,
+                _amount
+            )
+        );
+        strategy.rescueEth(_amount, _to);
+        vm.stopPrank();
+    }
+
+    function test_WhenTheCallToTheContractSucceeds(
+        uint256 _amount
+    )
+        external
+        whenToAddressIsAContract
+        givenTheTransactionSenderIsTheOwner
+    {
+        vm.assume(_amount > 0);
+        address _to = address(new TransferEthContract());
+        vm.deal(address(strategy), _amount);
+
+        vm.startPrank(strategy.owner());
+        vm.expectEmit(true, true, false, true, address(strategy));
+        emit EthRescuable.EthRescued(_to, _amount);
+        strategy.rescueEth(_amount, _to);
+        vm.stopPrank();
+
+        assertEq(_to.balance, _amount, "Invalid balance");
+    }
+}
